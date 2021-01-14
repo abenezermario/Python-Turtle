d707f5da5933d64b41b712794fd19e93092f3e06
abenezermario <58272831+abenezermario@users.noreply.github.com>
2021-01-13 20:23:21 -0600

    Update and rename CreateMenu.py to TurtleTest.py

---
+++ TurtleTest.py
@@ -0,0 +1,11 @@
+//Change the right degree to see amazing graphics with this simple pythonnnn
+import turtle
+
+abuchu = turtle.Turtle()
+
+abuchu.hideturtle()
+abuchu.speed(20)
+
+for i in range(350):
+    abuchu.forward(i)
+    abuchu.right(98)
